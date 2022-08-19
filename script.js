8774daba8c72b268f7695debc79d43f9061289d7
Lopez-CL <cllopez511@gmail.com>
2022-08-19 16:23:01 -0400

    Completed the Function Calc Challenge

---
+++ script.js
@@ -0,0 +1,50 @@
+function infoPop(){
+    alert("For this project I created a 4 founction calculator, meaning it can recieve numeric input, run basic arithmitic, clear, and output solution to arthmictic equation.");
+}
+
+setTimeout(infoPop, 100);
+var displayNum = document.querySelector("#display");
+var num1 = "";
+var num2 = "";
+var opperator = "";
+var sol = 0;
+
+function press(num){
+    num2 += num;
+    displayNum.innerText = num2;
+    console.log(num1);
+    
+}
+
+function setOP(opp){
+    opperator = opp;
+    console.log(opperator);
+    num1 = num2;
+    num2 = "";
+}
+function clr(){
+    num1 = "";
+    num2 = "";
+    opperator = "";
+    displayNum.innerText = "0";
+}
+
+function calculate(){
+    var a = parseInt(num1);
+    var b = parseInt(num2);
+    if (opperator === "+"){
+        sol = a + b;
+    }
+    if (opperator === "-"){
+        sol = a - b;
+    }
+    if (opperator === "*"){
+        sol = a * b;
+    }
+    if (opperator === "/"){
+        sol = a / b;
+    }
+    opperator = "";
+    num2 = sol;
+    displayNum.innerText = sol;
+}
