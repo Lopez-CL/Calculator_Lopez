7b52690099d7eb4c79ba7b450ad0ce9333571dcb
Lopez-CL <cllopez511@gmail.com>
2022-10-21 10:59:04 -0400

    adding changes made to pizzaria

--- script.js
+++ script.js
@@ -48,3 +48,17 @@ function calculate(){
     num2 = sol;
     displayNum.innerText = sol;
 }
+
+12. Shift Array Values
+shiftArrayValsLeft(arr)
+Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
+
+function shiftArrayValsLeft(arr){
+    for (var i = 0; i <= arr.length-1; i++){
+        arr[i] = arr[i + 1];
+    }
+    arr[arr.length - 1] = 0;
+    return arr;
+}
+
+console.log(shiftArrayValsLeft([1, 2, 3]));
